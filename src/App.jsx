a0ee31fc9995d2ace4f6c997051f3e1e681341f0
Md.Fazlul Haque <94878259+Tuhinbadhon@users.noreply.github.com>
2024-04-27 13:36:55 +0600

    first _commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h2 className="font-extrabold underline">Hello world</h2>
+      <h1 className="font-thin">hululi</h1>
+    </div>
+  );
+}
+
+export default App;
